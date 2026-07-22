35758a6522f86e5b1905206200e314549c92ae91
serdarevichar8 <serdarevichar@gmail.com>
2026-07-22 17:32:22 -0600

    remove unnecessary files

--- rocket_league_tcp_2.py
+++
@@ -1,9 +0,0 @@
-from tracker import RocketLeagueTracker
-
-if __name__ == "__main__":
-    tracker = RocketLeagueTracker(['tekjnbo','ilovethezoo46'])
-    try:
-        tracker.connect()
-    except KeyboardInterrupt:
-        tracker.save()
-        print("Stopped by user")
